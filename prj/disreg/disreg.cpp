811f7cad5f04a345f14b6628226c5a24c731880b
Florian Krull <flo.krull@gmail.com>
2018-02-14 22:44:05 +0100

    disreg: support input script

--- prj/disreg/disreg.cpp
+++ prj/disreg/disreg.cpp
@@ -7,24 +7,55 @@
 
 /*----------------------------------------------------------------------------*/
 
-std::vector<std::string> readList(const char * filename, char comment = '#') {
-  std::vector<std::string> ret;
-  std::ifstream file;
-  std::string line;
-
-  file.open(filename);
-  if (!file) {
-    fprintf(stderr, "error - cannot open file %s\n", filename);
-    exit(1);
-  }
-  while (getline((file), line)) {
-     if ((line.length() > 0) && (line.at(0) != comment)) {
-        ret.push_back(line);
-     }
-  }
-  file.close();
-  return ret;
+std::string removeExtension(const std::string & filename) {
+   std::string res = filename;
+   // cut before last "."
+   size_t pos_p = filename.find_last_of('.');
+   if (pos_p != filename.npos) {
+      res = res.substr(0, pos_p);
+   }
+   return res;
 }
+
+/*----------------------------------------------------------------------------*/
+
+std::string removePath(const std::string & filename) {
+   std::string res = filename;
+   // cut at last "/"
+   size_t pos_p = filename.find_last_of('/');
+   if (pos_p != filename.npos && (pos_p + 1) < res.size()) {
+      res = res.substr((pos_p + 1), res.size() - (pos_p + 1));
+   }
+   return res;
+}
+
+/*----------------------------------------------------------------------------*/
+
+class StrList {
+public:
+  void readList(const char * filename, char comment = '#') {
+    std::ifstream file;
+    std::string line;
+
+    file.open(filename);
+    if (!file) {
+      fprintf(stderr, "error - cannot open file %s\n", filename);
+      exit(1);
+    }
+    while (getline((file), line)) {
+       if ((line.length() > 0) && (line.at(0) != comment)) {
+         m_l.push_back(line);
+       }
+    }
+    file.close();
+  }
+
+  const std::vector<std::string> & get() const {
+    return m_l;
+  }
+private:
+  std::vector<std::string> m_l;
+};
 
 /*----------------------------------------------------------------------------*/
 
@@ -164,7 +195,7 @@ static float forbes(GCordsInfoCache & g1inf, GCordsInfoCache & g2inf) {
  /*
   * forbes = N * |A and B| / ( |A| * |B| )
   */
-  assert(g1inf.gcords()->chrinfo() == g2inf.gcords()->chrinfo());
+  //assert(g1inf.gcords()->chrinfo() == g2inf.gcords()->chrinfo());
   const ChrInfo & chrinfo = g1inf.gcords()->chrinfo();
   uint64_t n_ab = 0;
   uint64_t n_a = 0;
@@ -198,26 +229,132 @@ static float forbes(GCordsInfoCache & g1inf, GCordsInfoCache & g2inf) {
 
 /*----------------------------------------------------------------------------*/
 
-int main(int argc, char **argv) {
-  assert(argc == 3);
-  const char* fnl1 = argv[1];
-  const char* fnl2 = argv[2];
-
-  auto l1 = readList(fnl1);
-  auto l2 = readList(fnl2);
-
+static std::vector<std::vector<float>> disreg(const StrList &l1, const StrList &l2, const char *fmt1, const char *fmt2) {
   ChrInfoHg19 hg19;
-  for (uint32_t i = 0; i < l1.size(); i++) {
+  std::vector<std::vector<float>> mat;
+  mat.resize(l1.get().size());
+  for (uint32_t i = 0; i < l1.get().size(); i++) {
+    mat[i].resize(l2.get().size(), 0.f);
+#if 1
     GCords g1;
-    g1.read(l1[i].c_str(), "cse", 0, &hg19);
+    g1.read(l1.get()[i].c_str(), fmt1, 0, &hg19);
     GCordsInfoCache g1inf(&g1);
-    for (uint32_t j = 0; j < l2.size(); j++) {
+    for (uint32_t j = 0; j < l2.get().size(); j++) {
       GCords g2;
 
       GCordsInfoCache g2inf(&g2);
-      g2.read(l2[j].c_str(), "...........cs", 0, &hg19);
-      forbes(g2inf, g1inf);
+      g2.read(l2.get()[j].c_str(), fmt2, 0, &hg19);
+      //g2.expand(25000);
+      mat[i][j] = forbes(g2inf, g1inf);
     }
+#endif
+  }
+
+  FILE *f = fopen("/tmp/mat.txt", "w");
+  assert(f);
+  /* header */
+  for (uint32_t j = 0; j < l2.get().size(); j++) {
+    fprintf(f, "\t%s", removePath(removeExtension(l2.get()[j])).c_str());
+  }
+  fprintf(f, "\n");
+  /* data */
+  for (uint32_t i = 0; i < mat.size(); i++) {
+    fprintf(f, "%s", removePath(removeExtension(l1.get()[i])).c_str());
+    for (uint32_t j = 0; j < mat[i].size(); j++) {
+      fprintf(f, "%s%f", "\t", mat[i][j]);
+    }
+    fprintf(f, "\n");
+  }
+  fclose(f);
+  return mat;
+}
+
+/*----------------------------------------------------------------------------*/
+
+#include "../../int/objs_decl.h"
+
+OBJS_DECL_GCCLASS(StrList, StrList)
+
+/*----------------------------------------------------------------------------*/
+
+#include "../../int/command.h"
+#include "../../int/objspace.h"
+
+class CmdLoadStrList : public GcCommand {
+public:
+  CmdLoadStrList() {
+    addParam(GcCmdParam(PARAM_DST_STR,  GcCmdParam::PARAM_STRING, ""));
+    addParam(GcCmdParam(PARAM_FILE_STR, GcCmdParam::PARAM_STRING, ""));
+  }
+  const char* name() const {
+    return "load_strlist";
+  }
+  static std::string PARAM_DST_STR;
+  static std::string PARAM_FILE_STR;
+protected:
+  void executeChild(const char *, GcObjSpace *os);
+};
+
+std::string CmdLoadStrList::PARAM_DST_STR  = "dst";
+std::string CmdLoadStrList::PARAM_FILE_STR = "file";
+
+void CmdLoadStrList::executeChild(const char *, GcObjSpace *os) {
+  GcObjStrList *gcsl = new GcObjStrList();
+  gcsl->d()->readList(getParam(PARAM_FILE_STR)->valStr().c_str());
+  os->addObj(getParam(PARAM_DST_STR)->valStr(), gcsl);
+}
+
+/*----------------------------------------------------------------------------*/
+
+class CmdDisReg : public GcCommand {
+public:
+  CmdDisReg() {
+    addParam(GcCmdParam(PARAM_LIST1_STR, GcCmdParam::PARAM_STRING, ""));
+    addParam(GcCmdParam(PARAM_LIST2_STR, GcCmdParam::PARAM_STRING, ""));
+    addParam(GcCmdParam(PARAM_FORMAT1_STR, GcCmdParam::PARAM_STRING, "cse"));
+    addParam(GcCmdParam(PARAM_FORMAT2_STR, GcCmdParam::PARAM_STRING, "cse"));
+  }
+  const char* name() const {
+    return "disreg";
+  }
+  static std::string PARAM_LIST1_STR;
+  static std::string PARAM_LIST2_STR;
+  static std::string PARAM_FORMAT1_STR;
+  static std::string PARAM_FORMAT2_STR;
+protected:
+  void executeChild(const char *, GcObjSpace *os);
+};
+
+std::string CmdDisReg::PARAM_LIST1_STR  = "list1";
+std::string CmdDisReg::PARAM_LIST2_STR  = "list2";
+std::string CmdDisReg::PARAM_FORMAT1_STR  = "fmt1";
+std::string CmdDisReg::PARAM_FORMAT2_STR  = "fmt2";
+
+void CmdDisReg::executeChild(const char *, GcObjSpace *os) {
+  GcObjStrList *gcl1 = os->getObj<GcObjStrList>(getParam(PARAM_LIST1_STR)->valStr().c_str());
+  GcObjStrList *gcl2 = os->getObj<GcObjStrList>(getParam(PARAM_LIST2_STR)->valStr().c_str());
+  const char *fmt1 = getParam(PARAM_FORMAT1_STR)->valStr().c_str();
+  const char *fmt2 = getParam(PARAM_FORMAT2_STR)->valStr().c_str();
+  disreg(*gcl1->d(), *gcl2->d(), fmt1, fmt2);
+}
+
+/*----------------------------------------------------------------------------*/
+#include "../../int/scriptenv.h"
+int main(int argc, char **argv) {
+  if (argc == 3) {
+    const char* fnl1 = argv[1];
+    const char* fnl2 = argv[2];
+    StrList l1;
+    StrList l2;
+
+    l1.readList(fnl1);
+    l2.readList(fnl2);
+    disreg(l1, l2, "cse", "...........cs");
+  } else {
+    GcScriptEnv e;
+    e.addCmd(new CmdLoadStrList);
+    e.addCmd(new CmdDisReg);
+    e.runFile(argc >= 2 ? argv[1] : NULL);
   }
   printf("ok\n");
 }
