6b73620d3423595b1239ff16355e988991e44ba0
Florian Krull <flo.krull@gmail.com>
2018-03-11 19:19:36 +0100

    disreg: get SNP-associated genes with pos

--- prj/disreg/gwas_catalog.sh
+++ prj/disreg/gwas_catalog.sh
@@ -3,7 +3,7 @@
 trap 'exit' ERR
 
 declare -r SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-declare -r OUTDIR="$SCRIPTDIR"/../../../testdata/gwascat/
+declare -r OUTDIR="$SCRIPTDIR"/../../testdata/gwascat/
 
 mkdir -p "$OUTDIR"
 mkdir -p "$OUTDIR"/tmp
@@ -17,6 +17,7 @@ if [ ! -e $GWASCAT ]; then
 fi
 
 
+# separate SNPs by trait
 declare -ar TRAITS=( $( zcat $GWASCAT | tail -n +2 | cut -f 8 | sort | uniq | tr " " "_"  ) )
 rm -f $OUTDIR/list.txt
 for i in ${!TRAITS[@]}; do
@@ -34,7 +35,8 @@ for i in ${!TRAITS[@]}; do
   fi
 done
 
-# make table
+
+# make table of traits
 { 
   printf  "%s\t%s\t%s\n" path name num.SNPs 
   cat $OUTDIR/list.txt \
@@ -44,3 +46,39 @@ done
                           $( zcat $line | wc -l )
     done | sort -r -n -k 3,3
 } > $OUTDIR/tab.txt
+
+
+# get genes + gene postitions per trait
+
+declare -r REFGENE="$OUTDIR"/tmp/refGene.txt
+if [ ! -e $REFGENE ]; then
+  wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/refGene.txt.gz \
+    -O - | gunzip > "$REFGENE"
+fi
+
+{
+  printf  "%s\t%s\t%s\n" path name num.genes
+  while read file trait numsnps; do
+    outfile="$(readlink -f $OUTDIR/${trait}_genes.txt.gz)"
+    # get all gene symbols
+    genes="$(zcat $file | cut -f 14 | tr -d " " | tr "," "\n" | sort | uniq)"
+    if [ -z "$genes" ]; then
+      continue
+    fi
+    # grep for all gene symbols at col 13 (exact match in set a)
+    # filter chr1-22,X,Y
+    genepos="$(
+      awk 'FNR==NR{a[$1];next} ($13 in a)' <(echo "$genes") $REFGENE
+    )"
+
+    if [ -z "$genepos" ]; then
+      continue
+    fi
+
+    echo "$genepos" | gzip > "$outfile"
+
+    printf "%s\t%s\t%s\n" "$outfile" \
+                          "$trait" \
+                          "$(echo "$genepos" | wc -l)"
+  done < <(tail -n +2 $OUTDIR/tab.txt)
+} > $OUTDIR/tab_genes.txt
